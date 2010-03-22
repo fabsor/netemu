ef54af57f3f78d6390c25ffa08d01ea43c0f4ab3
Emil Nordén <emil@emil-laptop.(none)>
2010-03-22 19:00:48 +0100

    Added more method and enum definitions to the netlib header.

--- netlib/netemu_socket.h
+++ netlib/netemu_socket.h
@@ -47,11 +47,46 @@ extern "C" {
         SOCK_RDM,
         SOCK_STREAM
     };
+    
+    /* Constants defining flags for the send/recv calls. */
+    enum {
+        MSG_OOB,        // Sends Out-of-band data.
+        MSG_DONTROUTE   // Don't use local routing.
+    };
+
+    /* Structure describing a generic socket address.
+     * Hopefully this is identical on both the unix and win32 platform, not yet verified though. */
+    struct sockaddr {
+        unsigned short sa_family;   // THe address family of the socket.
+        char sa_data[14];           // Address info.
+    };
+
+    /* Type defining size of socket info */
+    typedef unsigned int socklen_t;
 
 
     /* Method definitions */
-    
+
+    /* Initializes the socket usage for the application on the platform */
+    int init_network();
+
+    /* Creates a new NETEMU_SOCKET */
     NETEMU_SOCKET netemu_socket(int address_family, int socket_type);
+
+    /* Binds a given NETEMU_SOCKET to an address) */
+    int bind(NETEMU_SOCKET socket, const struct sockaddr* address, socklen_t address_len);
+
+    /* Places a socket in a state where it listens for incoming connection attempts */
+    int listen(NETEMU_SOCKET socket, int backlog);
+
+    /* Accepts a connection attempt made on the listening socket. */
+    NETEMU_SOCKET accept(NETEMU_SOCKET socket, struct sockaddr* address, socklen_t* address_len);
+
+    /* Sends data through a connected socket. */
+    int send(NETEMU_SOCKET socket, const char* buffer, int len, int flags);
+
+    /* Sends data to a specific destination */
+    int sendto(NETEMU_SOCKET socket, const char* buffer, int len, int flags, const struct sockaddr* dest_address, socklen_t* address_len);
 
 
 
