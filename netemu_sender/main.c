ff8ac113d5ab20648f72e6ccd35aa9bdb9d2a709
unknown <Emil@.(none)>
2010-03-29 16:23:35 +0200

    sender test implementation

---
+++ netemu_sender/main.c
@@ -0,0 +1,51 @@
+#include "..\netemu\netlib\netemu_socket.h"
+#include <stdio.h>
+
+void send_data(NETEMU_SOCKET);
+
+int main()
+{
+	int error;
+	NETEMU_SOCKET socket;
+	struct netemu_sockaddr_in addr;
+	addr.addr = htonl(INADDR_ANY);
+	addr.family = NETEMU_AF_INET;
+	addr.port = 0;
+
+	printf("SENDER");
+
+	error = netemu_init_network();
+	if(error != 0) {
+		printf("INIT: %i", netemu_get_last_error());
+	}
+
+	socket = netemu_socket(NETEMU_AF_INET, NETEMU_SOCK_DGRAM);
+	if(socket == INVALID_SOCKET) {
+		printf("OH NOES!\n %i", netemu_get_last_error());
+	}
+
+	error = netemu_bind(socket, netemu_prepare_net_addr(&addr), sizeof(addr));
+
+	if(error != 0) {
+		printf("Error: %d", netemu_get_last_error());
+	}
+
+	send_data(socket);
+	return 0;
+}
+
+void send_data(NETEMU_SOCKET socket) {
+	char c;
+	struct netemu_sockaddr_in addr;
+	int data_received;
+	int size = 22;
+
+	addr.addr = htonl(INADDR_LOOPBACK);
+	addr.family = NETEMU_AF_INET;
+	addr.port = 27015;
+	
+	while(1) {
+		c = (char)getchar();
+		netemu_sendto(socket, &c, 1, 0, netemu_prepare_net_addr(&addr), sizeof(addr));
+	}
+}
