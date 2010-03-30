0e273b1448c9bb72b8ead76a9269830e4d3e9610
Fabian Sörqvist <fabian.sorqvist@gmail.com>
2010-03-30 08:50:27 +0200

    makefile for linux x86-32

---
+++ netlib/linux-x86-32/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../netemu_list.c \
+../netemu_socket_nix.c 
+
+OBJS += \
+./netemu_list.o \
+./netemu_socket_nix.o 
+
+C_DEPS += \
+./netemu_list.d \
+./netemu_socket_nix.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C Compiler'
+	gcc -D_NIX -O0 -g3 -pedantic -Wall -c -fmessage-length=0 -m32 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
