24a007280b3c11d1c2c3f3a7eca49e05f972eb42
Fabian Sörqvist <fabian.sorqvist@gmail.com>
2010-03-31 17:20:36 +0200

    added comments for netemu_had

--- netlib/headers/netemu_thread.h
+++ netlib/headers/netemu_thread.h
@@ -5,26 +5,56 @@
  *      Author: fabian
  */
 
+/**
+ * @file
+ * This header defines an interface which clients can use to get cross platform multithreading.
+ */
 #ifndef NETEMU_THREAD_H_
 #define NETEMU_THREAD_H_
 
-typedef int netemu_thread;
+/* TODO: Fix this for windows as well. */
+typedef unsigned long int netemu_thread;
 
-/* Since the windows threads are using the very microsofty WINAPI, we need to typedef a little =) */
-
+/* TODO: make sure this is a good type to use. */
 typedef int NETEMU_MUTEX;
 
+/**
+ * Start a new thread.
+ * @param netemu_thread* identifier an identifier that can be used to identify the thread.
+ * @param callback a function callback that will be called when the thread has started.
+ * @return 0 if the thread was created successfully, an error code otherwise.
+ */
 int netemu_thread_new(netemu_thread* identifier, void (*start_fn) (void *), void* arg);
 
-int netemu_thread_exit(netemu_thread* identifier);
+/**
+ * Exit the current thread.
+ * @return 0 if everything went ok.
+ */
+int netemu_thread_exit();
 
+/**
+ * Create a new mutex lock.
+ * @return an identifier for the mutex lock.
+ */
 NETEMU_MUTEX netemu_thread_mutex_create();
 
-void netemu_thread_mutex_lock(NETEMU_MUTEX* mutex);
+/**
+ * Lock a mutex lock.
+ * @param NETEMU_MUTEX identifier the identifier of the mutex lock.
+ */
+void netemu_thread_mutex_lock(NETEMU_MUTEX* mutex_identifier);
 
-void netemu_thread_mutex_release(NETEMU_MUTEX* mutex);
+/**
+ * Release a lock.
+ * @param NETEMU_MUTEX identifier the identifier for this lock.
+ */
+void netemu_thread_mutex_release(NETEMU_MUTEX* mutex_identifier);
 
-void netemu_thread_mutex_destroy(NETEMU_MUTEX* mutex);
+/**
+ * Destroy a mutex lock.
+ * @param NETEMU_MUTEX the identifier of the lock.
+ */
+void netemu_thread_mutex_destroy(NETEMU_MUTEX* mutex_identifier);
 
 
 
