53c46a7dbf15abf6ad19cd8d3a284237ea4f0ced
unknown <Emil@.(none)>
2010-03-31 14:42:09 +0200

    Made changes to netemu_thread_win.c

--- netlib/netemu_thread_win.c
+++ netlib/netemu_thread_win.c
@@ -3,3 +3,7 @@
 int netemu_thread_new(netemu_thread* identifier, unsigned long (NETEMU_API *start_fn) (void *), void *arg) {
 	return CreateThread(NULL, 0, start_fn, arg, 0, identifier);
 }
+
+int netemu_thread_stop(netemu_thread* identifier) {
+	return (int)TerminateThread(identifier, 0);
+}
