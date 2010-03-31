209c4cebc5f9a915bc1de335cdbfa14996015c89
Fabian Sörqvist <fabian.sorqvist@gmail.com>
2010-03-31 14:39:57 +0200

    First compiling version of list

--- netlib/netemu_thread_nix.c
+++ netlib/netemu_thread_nix.c
@@ -4,16 +4,36 @@
  *  Created on: 30 mar 2010
  *      Author: fabbe
  */
+
+/**
+ * @file
+ * This is the nix implementation of netemu_thread.h, implemented with phtread.
+ */
+#include "headers/netemu_thread.h"
 #include "pthread.h"
 
-int netemu_thread_new(netemu_thread* identifier, NETEMU_API *(*start_fn) (void *)) {
-	pthread_create(identifier,NULL,start_fn,identifier);
+/*
+struct threadargs {
+	unsigned long (NETEMU_API *start_fn) (void *);
+};
+
+void* _netemu_thread_callback();
+
+int netemu_thread_new(netemu_thread* identifier, ) {
+	return pthread_create(identifier,0,_netemeu_thread_callback,identifier);
 }
 
-int netemu_thread_stop(netemu_thread* identifier) {
+int netemu_thread_stop(netemu_thread *identifier) {
 	pthread_exit(identifier);
+	return 1;
+}
+
+void* _netemeu_thread_callback() {
+	int hej = 1;
+	start_fn(&hej);
 }
 
 int netemu_thread_mutex_lock();
 
 int netemu_thread_mutex_release(void* memory);
+*/
--- netlib/netemu_thread_win.c
+++ netlib/netemu_thread_win.c
@@ -3,3 +3,4 @@
 int netemu_thread_new(netemu_thread* identifier, unsigned long (NETEMU_API *start_fn) (void *), void *arg) {
 	return CreateThread(NULL, 0, start_fn, arg, 0, identifier);
 }
+
