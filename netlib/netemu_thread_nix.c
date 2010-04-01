8c13ded5ebc8cc083389b16ddb9289343bb58c08
Fabian Sörqvist <fabian.sorqvist@gmail.com>
2010-04-01 08:25:28 +0200

    changed nix implementation of netemu_thread.h to conform with new standards in header file

--- netlib/netemu_thread_nix.c
+++ netlib/netemu_thread_nix.c
@@ -11,9 +11,13 @@
  * @author Fabian Sörqvist <fabian.sorqvist@gmail.com>
  */
 #include <stdlib.h>
-#include "headers/netemu_thread.h"
 #include "pthread.h"
+#include "headers/netemu_thread.h"
 #include "headers/netemu_list.h"
+
+struct netemu_mutex{
+	pthread_mutex_t* mutex;
+};
 
 /* TODO: Tweak this value. We try a big one initially. */
 #define NETEMU_THREAD_MUTEX_LIST_INITIAL_SIZE 100;
@@ -21,17 +25,11 @@
 void *_netemu_thread_callback(void* arg);
 
 /*! This struct is used for passing arguments to the thread. */
-struct netemu_thread_struct {
+struct netemu_thread_args {
 	netemu_thread* identifier;
 	void (*start_fn) (void *);
 	void* arg;
 };
-
-/*! We save all mutex locks created by a program here. That way, we can make sure there are no duplicate mutex locks. */
-struct netemu_list* mutexes;
-
-/*! We need to protect the mutexes list from being accessed simultaneusly. We will need a lock for controlling this. */
-pthread_mutex_t mutex_lock;
 
 /**
  * Start a new thread.
@@ -48,8 +46,8 @@ int netemu_thread_new(netemu_thread* identifier, void (*start_fn) (void *), void
  * @param arg this will always be a netemu_thread_struct.
  */
 void *_netemu_thread_callback(void* arg) {
-	struct netemu_thread_struct *arg_struct;
-	arg_struct = (struct netemu_thread_struct*)arg;
+	struct netemu_thread_args *arg_struct;
+	arg_struct = (struct netemu_thread_args*)arg;
 	/*Start the thread by calling the function passed by the user. */
 	arg_struct->start_fn(arg);
 	return NULL;
@@ -67,60 +65,36 @@ int netemu_thread_exit() {
  * Create a new mutex lock.
  * @return an identifier for the mutex lock.
  */
-NETEMU_MUTEX netemu_thread_mutex_create() {
-	pthread_mutex_t* mutex;
-	/* Make sure that the list is instantiated. */
-	if (mutexes == NULL) {
-		mutexes = netemu_list_new(sizeof(pthread_mutex_t), 100);
-		pthread_mutex_init(&mutex_lock, NULL);
-	}
+netemu_mutex netemu_thread_mutex_create() {
+	netemu_mutex mutex_struct;
 	/* We need a pointer to this mutex, so we can save it off in our list and send the index back to the user. Let's allocate it on the heap.*/
-	mutex = malloc(sizeof(pthread_mutex_t));
-	pthread_mutex_init(mutex, NULL);
-	/* Now, let's save it in our list of mutex locks. We also need to use our lock. */
-	pthread_mutex_lock(&mutex_lock);
-	netemu_list_add(mutexes,mutex);
-	pthread_mutex_unlock(&mutex_lock);
-
-	/* We just return the index of the added mutex. */
-	return mutexes->count-1;
+	mutex_struct = malloc(sizeof(struct netemu_mutex));
+	mutex_struct->mutex = malloc(sizeof(pthread_mutex_t));
+	pthread_mutex_init(mutex_struct->mutex, NULL);
+	return mutex_struct;
 }
 
 /**
  * Lock a mutex lock.
  * @param NETEMU_MUTEX identifier the identifier of the mutex lock.
  */
-void netemu_thread_mutex_lock(NETEMU_MUTEX *mutex_identifier) {
-	pthread_mutex_t* mutex;
-	pthread_mutex_lock(&mutex_lock);
-	mutex = mutexes->elements[*mutex_identifier];
-	pthread_mutex_unlock(&mutex_lock);
-	pthread_mutex_lock(mutex);
+void netemu_thread_mutex_lock(netemu_mutex mutex_identifier) {
+	pthread_mutex_lock(mutex_identifier->mutex);
 }
 
 /**
  * Release a lock.
  * @param NETEMU_MUTEX identifier the identifier for this lock.
  */
-void netemu_thread_mutex_release(NETEMU_MUTEX *mutex_identifier) {
-	pthread_mutex_t* mutex;
-	/* We need to acquire a lock in order to be able to modify the list. */
-	pthread_mutex_lock(&mutex_lock);
-	mutex = mutexes->elements[*mutex_identifier];
-	pthread_mutex_unlock(&mutex_lock);
-	pthread_mutex_unlock(mutex);
+void netemu_thread_mutex_release(netemu_mutex mutex_identifier) {
+	pthread_mutex_unlock(mutex_identifier->mutex);
 }
 
 /**
  * Destroy a mutex lock.
  * @param NETEMU_MUTEX the identifier of the lock.
  */
-void netemu_thread_mutex_destroy(NETEMU_MUTEX *mutex_identifier) {
-	pthread_mutex_t* mutex;
-	pthread_mutex_lock(&mutex_lock);
-	mutex = mutexes->elements[*mutex_identifier];
-	netemu_list_remove_at(mutexes,*mutex_identifier);
-	pthread_mutex_unlock(&mutex_lock);
-	pthread_mutex_destroy(mutex);
-	free(mutex);
+void netemu_thread_mutex_destroy(netemu_mutex mutex_identifier) {
+	pthread_mutex_destroy(mutex_identifier->mutex);
+	free(mutex_identifier);
 }
