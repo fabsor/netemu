9c4f7a0cbfccd4f6506c7244cd97f5796e673739
unknown <Emil@.(none)>
2010-04-01 11:16:56 +0200

    Made some changes to netemu_thread.h and began implementing the windows implementation

--- netlib/headers/netemu_thread.h
+++ netlib/headers/netemu_thread.h
@@ -1,65 +1,69 @@
 /*
- * netemu_thread.h
- *
- *  Created on: 30 mar 2010
- *      Author: fabian
- */
+* netemu_thread.h
+*
+*  Created on: 30 mar 2010
+*      Author: fabian
+*/
 
 /**
- * @file
- * This header defines an interface which clients can use to get cross platform multithreading.
- */
+* @file
+* This header defines an interface which clients can use to get cross platform multithreading.
+*/
 
 #ifndef NETEMU_THREAD_H_
 #define NETEMU_THREAD_H_
 #ifdef	__cplusplus
 extern "C" {
 #endif
-#include "pthread.h"
-/* TODO: Fix this for windows as well. */
-typedef unsigned long int netemu_thread;
 
-/* TODO: make sure this is a good type to use. */
-typedef struct netemu_mutex* netemu_mutex;
+#ifdef _NIX
+	typedef unsigned long int netemu_thread;
+#else
+#include <Windows.h>
+typedef HANDLE netemu_thread;
+#endif
+
+	/* TODO: make sure this is a good type to use. */
+	typedef struct netemu_mutex* netemu_mutex;
 
 
-/**
- * Start a new thread.
- * @param netemu_thread* identifier an identifier that can be used to identify the thread.
- * @param callback a function callback that will be called when the thread has started.
- * @return 0 if the thread was created successfully, an error code otherwise.
- */
-int netemu_thread_new(netemu_thread* identifier, void (*start_fn) (void *), void* arg);
+	/**
+	* Start a new thread.
+	* @param netemu_thread* identifier an identifier that can be used to identify the thread.
+	* @param callback a function callback that will be called when the thread has started.
+	* @return 0 if the thread was created successfully, an error code otherwise.
+	*/
+	int netemu_thread_new(netemu_thread* identifier, void (*start_fn) (void *), void* arg);
 
-/**
- * Exit the current thread.
- * @return 0 if everything went ok.
- */
-int netemu_thread_exit();
+	/**
+	* Exit the current thread.
+	* @return 0 if everything went ok.
+	*/
+	int netemu_thread_exit();
 
-/**
- * Create a new mutex lock.
- * @return an identifier for the mutex lock.
- */
-netemu_mutex netemu_thread_mutex_create();
+	/**
+	* Create a new mutex lock.
+	* @return an identifier for the mutex lock.
+	*/
+	netemu_mutex netemu_thread_mutex_create();
 
-/**
- * Lock a mutex lock.
- * @param NETEMU_MUTEX identifier the identifier of the mutex lock.
- */
-void netemu_thread_mutex_lock(netemu_mutex mutex_identifier);
+	/**
+	* Lock a mutex lock.
+	* @param NETEMU_MUTEX identifier the identifier of the mutex lock.
+	*/
+	void netemu_thread_mutex_lock(netemu_mutex mutex_identifier);
 
-/**
- * Release a lock.
- * @param NETEMU_MUTEX identifier the identifier for this lock.
- */
-void netemu_thread_mutex_release(netemu_mutex mutex_identifier);
+	/**
+	* Release a lock.
+	* @param NETEMU_MUTEX identifier the identifier for this lock.
+	*/
+	void netemu_thread_mutex_release(netemu_mutex mutex_identifier);
 
-/**
- * Destroy a mutex lock.
- * @param NETEMU_MUTEX the identifier of the lock.
- */
-void netemu_thread_mutex_destroy(netemu_mutex mutex_identifier);
+	/**
+	* Destroy a mutex lock.
+	* @param NETEMU_MUTEX the identifier of the lock.
+	*/
+	void netemu_thread_mutex_destroy(netemu_mutex mutex_identifier);
 
 #ifdef	__cplusplus
 }
--- netlib/netemu_thread_win.c
+++ netlib/netemu_thread_win.c
@@ -1,10 +1,24 @@
 #include "netemu_thread.h"
 
-int netemu_thread_new(netemu_thread* identifier, unsigned long (NETEMU_API *start_fn) (void *), void *arg) {
-	return CreateThread(NULL, 0, start_fn, arg, 0, identifier);
+struct netemu_threadstart{
+	void (*start_fn) (void *);
+	void *arguments;
+};
+
+DWORD WINAPI _netemu_thread_callback(void *arg);
+
+int netemu_thread_new(netemu_thread* identifier, void (*start_fn) (void *), void* arg) {
+	struct netemu_threadstart data;
+	data.arguments = arg;
+	data.start_fn = start_fn;
+	//return CreateThread(NULL, 0, start_fn, arg, 0, identifier);
 }
 
 int netemu_thread_stop(netemu_thread* identifier) {
-	unsigned long long i;
 	return (int)TerminateThread(identifier, 0);
+}
+
+DWORD WINAPI _netemu_thread_callback(void *arg) {
+	struct netemu_threadstart *data = (struct netemu_threadstart *)arg;
+	data->start_fn(data->arguments);
 }
