a4b7c9a7c89aa64413f2ca9ae46c56a65ef6b4b0
Fabian Sörqvist <fabian.sorqvist@gmail.com>
2010-04-06 09:42:51 +0200

    changed snprintf to memcpy

--- netemu_sender/protocol/transport.c
+++ netemu_sender/protocol/transport.c
@@ -5,30 +5,38 @@
  *      Author: fabbe
  */
 #include <stdlib.h>
-#include <stdio.h>
+#include <string.h>
 #include "transport.h"
 #include "application.h"
 
 char* netemu_transport_pack(struct protocol_message **messages, int count) {
-	struct transport_packet packet;
-	struct transport_instruction* instruction;
-	char *buffer;
+	void *buffer;
 	int i;
+	int index;
 	int total_size;
-
+	int pos;
 	total_size = sizeof(int);
-
 	for(i = 0; i < count; i++) {
 		total_size += messages[i]->total_size+sizeof(int)*2;
 	}
 	buffer = malloc(total_size);
-	snprintf(buffer,sizeof(int),"%d",count);
-
+	memcpy(buffer,(void *)&count,sizeof(int));
+	pos = sizeof(int);
 	for(i = 0; i < count; i++) {
-		snprintf(buffer,sizeof(int),"%d",i+1);
-		snprintf(buffer,sizeof(int),"%d",messages[i]->total_size);
-		snprintf(buffer,sizeof(int),"%s",messages[i]->body);
+		index = i+1;
+		memcpy((void *)(buffer+pos),(void *)&index,sizeof(int));
+		pos += sizeof(int);
+		memcpy((void *)(buffer+pos),(void *)&messages[i]->total_size,sizeof(int));
+		pos += sizeof(int);
+		memcpy((void *)(buffer+pos),messages[i]->body,sizeof(int));
+		pos += sizeof(messages[i]->total_size);
 	}
+	return (char*)buffer;
+}
 
-	return buffer;
+struct transport_packet* netemu_transport_unpack(void* data) {
+	struct tranport_packet* transport_packet;
+	struct transport_instruction instruction;
+
+
 }
