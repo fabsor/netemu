04acf06478b3ce1c59a539d640085cca4f2176b8
fabian.sorqvist@gmail.com <fabian.sorqvist@gmail.com>
2010-04-09 09:51:10 +0200

    a lot

--- netlib/linux-x86-64/subdir.mk
+++ netlib/linux-x86-64/subdir.mk
@@ -6,24 +6,27 @@
 C_SRCS += \
 ../netemu_list.c \
 ../netemu_socket_nix.c \
-../netemu_thread_nix.c 
+../netemu_thread_nix.c \
+../netemu_util_nix.c 
 
 OBJS += \
 ./netemu_list.o \
 ./netemu_socket_nix.o \
-./netemu_thread_nix.o 
+./netemu_thread_nix.o \
+./netemu_util_nix.o 
 
 C_DEPS += \
 ./netemu_list.d \
 ./netemu_socket_nix.d \
-./netemu_thread_nix.d 
+./netemu_thread_nix.d \
+./netemu_util_nix.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
 %.o: ../%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -D_NIX -I"/home/fabian/workspace/netemu/netlib/headers" -O0 -g3 -pedantic -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	gcc -D_NIX -I"/home/fabbe/workspace/netemu/netlib/headers" -O0 -g3 -pedantic -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
