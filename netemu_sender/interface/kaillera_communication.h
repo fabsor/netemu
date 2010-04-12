2dbdea1cd2b31bb0d67fcdebd94d601ada81212e
Fabian Sörqvist <fabian.sorqvist@gmail.com>
2010-04-12 16:21:34 +0200

    Async implementation of connect

--- netemu_sender/interface/kaillera_communication.h
+++ netemu_sender/interface/kaillera_communication.h
@@ -12,7 +12,7 @@
 #include "../network/netemu_receiver.h"
 #include "../protocol/communication.h"
 #include "../network/netemu_sender.h"
-
+#include "../server_connection.h"
 
 struct netemu_communication_server {
 	char *name;
@@ -27,8 +27,8 @@ struct netemu_communication_server* kaillera_communication_get_server_list();
  */
 void kaillera_communication_get_server_list_async(void (*listReceivedFn(struct netemu_communication_server *server)));
 
-void kaillera_communication_connect(struct netemu_sockaddr_in *addr);
+struct server_connection* kaillera_communication_connect(struct netemu_sockaddr_in *addr);
 
-void kaillera_communication_connect_async(struct netemu_sockaddr_in *addr, void (*ConnectionReceivedFn)(int port));
+void kaillera_communication_connect_async(struct netemu_sockaddr_in *addr, void (*ConnectionReceivedFn)(int status, struct server_connection*));
 
 #endif /* COMMUNICATION_STATE_H_ */
