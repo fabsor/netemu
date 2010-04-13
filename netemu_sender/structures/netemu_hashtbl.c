173c3cc34368ea008604c94fbb18c1079d00aea5
Emil Norden <emil_norden@hotmail.com>
2010-04-13 11:08:18 +0200

    hash table data structure

---
+++ netemu_sender/structures/netemu_hashtbl.c
@@ -0,0 +1,188 @@
+/* Copyright (c) 2010 the authors listed at the following URL, and/or
+the authors of referenced articles or incorporated external code:
+http://en.literateprograms.org/Hash_table_(C)?action=history&offset=20100122102906
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Retrieved from: http://en.literateprograms.org/Hash_table_(C)?oldid=16632
+*/
+
+#include "netemu_hashtbl.h"
+
+#include <string.h>
+#include <stdio.h>
+
+static char *mystrdup(const char *s)
+{
+	char *b;
+	if(!(b=malloc(strlen(s)+1))) return NULL;
+	strcpy(b, s);
+	return b;
+}
+
+
+static hash_size def_hashfunc(const void *key)
+{
+	char* char_key = (char*) key;
+	hash_size hash=0;
+	
+	while(*char_key) hash+=(unsigned char)*char_key++;
+
+	return hash;
+}
+
+
+
+NETEMU_HASHTBL *netemu_hashtbl_create(hash_size size, hash_size (*hashfunc)(const void *))
+{
+	NETEMU_HASHTBL *hashtbl;
+
+	if(!(hashtbl=malloc(sizeof(NETEMU_HASHTBL)))) return NULL;
+
+	if(!(hashtbl->nodes=calloc(size, sizeof(struct hashnode_s*)))) {
+		free(hashtbl);
+		return NULL;
+	}
+
+	hashtbl->size=size;
+
+	if(hashfunc) hashtbl->hashfunc=hashfunc;
+	else hashtbl->hashfunc=def_hashfunc;
+
+	return hashtbl;
+}
+
+
+void netemu_hashtbl_destroy(NETEMU_HASHTBL *hashtbl)
+{
+	hash_size n;
+	struct hashnode_s *node, *oldnode;
+	
+	for(n=0; n<hashtbl->size; ++n) {
+		node=hashtbl->nodes[n];
+		while(node) {
+			free(node->key);
+			oldnode=node;
+			node=node->next;
+			free(oldnode);
+		}
+	}
+	free(hashtbl->nodes);
+	free(hashtbl);
+}
+
+
+int netemu_hashtbl_insert(NETEMU_HASHTBL *hashtbl, const void *key, void *data)
+{
+	struct hashnode_s *node;
+	hash_size hash=hashtbl->hashfunc(key)%hashtbl->size;
+
+
+/*	fprintf(stderr, "hashtbl_insert() key=%s, hash=%d, data=%s\n", key, hash, (char*)data);*/
+
+	node=hashtbl->nodes[hash];
+	while(node) {
+		if(!strcmp(node->key, key)) {
+			node->data=data;
+			return 0;
+		}
+		node=node->next;
+	}
+
+
+	if(!(node=malloc(sizeof(struct hashnode_s)))) return -1;
+	if(!(node->key=mystrdup(key))) {
+		free(node);
+		return -1;
+	}
+	node->data=data;
+	node->next=hashtbl->nodes[hash];
+	hashtbl->nodes[hash]=node;
+
+
+	return 0;
+}
+
+
+int netemu_hashtbl_remove(NETEMU_HASHTBL *hashtbl, const void *key)
+{
+	struct hashnode_s *node, *prevnode=NULL;
+	hash_size hash=hashtbl->hashfunc(key)%hashtbl->size;
+
+	node=hashtbl->nodes[hash];
+	while(node) {
+		if(!strcmp(node->key, key)) {
+			free(node->key);
+			if(prevnode) prevnode->next=node->next;
+			else hashtbl->nodes[hash]=node->next;
+			free(node);
+			return 0;
+		}
+		prevnode=node;
+		node=node->next;
+	}
+
+	return -1;
+}
+
+
+void *netemu_hashtbl_get(NETEMU_HASHTBL *hashtbl, const void *key)
+{
+	struct hashnode_s *node;
+	hash_size hash=hashtbl->hashfunc(key)%hashtbl->size;
+
+/*	fprintf(stderr, "hashtbl_get() key=%s, hash=%d\n", key, hash);*/
+
+	node=hashtbl->nodes[hash];
+	while(node) {
+		if(!strcmp(node->key, key)) return node->data;
+		node=node->next;
+	}
+
+	return NULL;
+}
+
+int netemu_hashtbl_resize(NETEMU_HASHTBL *hashtbl, hash_size size)
+{
+	NETEMU_HASHTBL newtbl;
+	hash_size n;
+	struct hashnode_s *node,*next;
+
+	newtbl.size=size;
+	newtbl.hashfunc=hashtbl->hashfunc;
+
+	if(!(newtbl.nodes=calloc(size, sizeof(struct hashnode_s*)))) return -1;
+
+	for(n=0; n<hashtbl->size; ++n) {
+		for(node=hashtbl->nodes[n]; node; node=next) {
+			next = node->next;
+			netemu_hashtbl_insert(&newtbl, node->key, node->data);
+			netemu_hashtbl_remove(hashtbl, node->key);
+			
+		}
+	}
+
+	free(hashtbl->nodes);
+	hashtbl->size=newtbl.size;
+	hashtbl->nodes=newtbl.nodes;
+
+	return 0;
+}
+
