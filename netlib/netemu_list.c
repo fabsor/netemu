724abac95169865fb8730216afc2a8e70b2b0de8
Fabian Sörqvist <fabian.sorqvist@gmail.com>
2010-04-15 16:46:08 +0200

    Reverted som changes to the list

--- netlib/netemu_list.c
+++ netlib/netemu_list.c
@@ -3,56 +3,56 @@
 
 struct _netemu_list_internal {
 	size_t element_size;
-    int ( * comparator ) ( const void *, const void * );
-    int size;
+	int (* comparator)(const void *, const void *);
+	int size;
 };
 
 void _netemu_enlarge_list(struct netemu_list* list, int size);
 int _netemu_list_search_linear(struct netemu_list* list, void* element);
 
 struct netemu_list* netemu_list_new(int element_size, int count) {
-  struct netemu_list* list;
-  struct _netemu_list_internal* intern;
+	struct netemu_list* list;
+	struct _netemu_list_internal* intern;
 
-  intern = malloc(sizeof(struct _netemu_list_internal));
-  list = malloc(sizeof(struct netemu_list));
-  intern->size = count;
-  intern->element_size = element_size;
-  list->elements = malloc(element_size*intern->size);
-  list->count = 0;
-  list->_intern = intern;
-  return list;
+	intern = malloc(sizeof(struct _netemu_list_internal));
+	list = malloc(sizeof(struct netemu_list));
+	intern->size = count;
+	intern->element_size = element_size;
+	list->elements = malloc(element_size * intern->size);
+	list->count = 0;
+	list->_intern = intern;
+	return list;
 }
 
 void netemu_list_add(struct netemu_list* list, void* element) {
-    /* Enlarge the array if necessary. */
-	if (list->_intern->size <= list->count){
-        _netemu_enlarge_list(list,10);
-    }
-    list->elements[list->count] = element;
-    list->count++;
-    list->sorted = 0;
+	/* Enlarge the array if necessary. */
+	if (list->_intern->size <= list->count) {
+		_netemu_enlarge_list(list, 10);
+	}
+	list->elements[list->count] = element;
+	list->count++;
+	list->sorted = 0;
 }
 
 void _netemu_enlarge_list(struct netemu_list* list, int size) {
-    int i;
-    void* elements;
-    list->_intern->size += size;
-    elements = malloc(list->_intern->element_size*list->_intern->size);
-	memcpy(elements, list->elements, list->count * (list->_intern->element_size));
+	void* elements;
+	list->_intern->size += size;
+	elements = malloc(list->_intern->element_size * list->_intern->size);
+	memcpy(elements, list->elements, list->count
+			* (list->_intern->element_size));
 
-    free(list->elements);
-    list->elements = elements;
+	free(list->elements);
+	list->elements = elements;
 }
 
 int netemu_list_contains(struct netemu_list* list, void* element) {
-	return _netemu_list_search_linear(list,element);
+	return _netemu_list_search_linear(list, element);
 }
 
 int _netemu_list_search_linear(struct netemu_list* list, void* element) {
 	int i;
 	int index = -1;
-	for (i = 0; i < list->count; i++){
+	for (i = 0; i < list->count; i++) {
 		if (list->elements[i] == element) {
 			return index;
 		}
@@ -60,23 +60,23 @@ int _netemu_list_search_linear(struct netemu_list* list, void* element) {
 	return -1;
 }
 
-int netemu_list_remove(struct netemu_list* list, void* element){
+int netemu_list_remove(struct netemu_list* list, void* element) {
 	int index;
 	/*TODO: Add binary search when sorting is available. */
-	index = _netemu_list_search_linear(list,element);
+	index = _netemu_list_search_linear(list, element);
 	if (index == -1) {
 		return index;
 	}
-	return netemu_list_remove_at(list,index);
+	return netemu_list_remove_at(list, index);
 }
 
-int netemu_list_remove_at(struct netemu_list* list,int index) {
+int netemu_list_remove_at(struct netemu_list* list, int index) {
 	int i;
 	if (list->count < index) {
 		return -1;
 	}
-	for (i = index; i < list->count-1; i++) {
-		list->elements[i] = list->elements[i+1];
+	for (i = index; i < list->count - 1; i++) {
+		list->elements[i] = list->elements[i + 1];
 	}
 	list->count--;
 	return 1;
@@ -88,7 +88,8 @@ int netemu_list_remove_at(struct netemu_list* list,int index) {
  * @param struct netemu_list* list the list to register the function to.
  * @param the comparator function.
  */
-void netemu_list_register_sort_fn(struct netemu_list* list, int ( * comparator ) ( const void *, const void * )) {
+void netemu_list_register_sort_fn(struct netemu_list* list, int(* comparator)(
+		const void *, const void *)) {
 	list->_intern->comparator = comparator;
 }
 
@@ -96,14 +97,15 @@ void netemu_list_register_sort_fn(struct netemu_list* list, int ( * comparator )
  * Sort the list. To use this function, you need to register a sorting function.
  * @return 1 if the sorting was successful, 0 if you haven't registered a sorting function.
  */
-int netemu_list_sort(struct netemu_list* list){
+int netemu_list_sort(struct netemu_list* list) {
 	/* If this list is sorted, then there's nothing to be done. */
-	if(list->sorted)
+	if (list->sorted)
 		return 1;
 
 	/* We can't sort if we don't have a comparator. */
-	if(list->_intern->comparator != 0) {
-		qsort(list->elements[0],list->count,list->_intern->element_size,list->_intern->comparator);
+	if (list->_intern->comparator != 0) {
+		qsort(list->elements[0], list->count, list->_intern->element_size,
+				list->_intern->comparator);
 		list->sorted = 1;
 		return 1;
 	}
@@ -135,4 +137,14 @@ void* netemu_list_get(struct netemu_list* list, int index) {
 		return NULL;
 	}
 	return list->elements[index];
+}
+
+void netemu_list_clear(struct netemu_list* list) {
+	int i;
+	for(i = 0; i < list->count; i++) {
+		free(list->elements[i]);
+	}
+	free(list->elements);
+	list->elements = malloc(sizeof(void*)*20);
+	list->count = 0;
 }
