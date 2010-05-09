6818fd71d6cb466dcd0857c45c26feec05649da4
Emil Nordén <emil@emil-laptop.(none)>
2010-05-09 21:30:46 +0200

    Added the function declaration for _netemu_respond_to_user_leave

--- netemu_sender/responses.h
+++ netemu_sender/responses.h
@@ -15,6 +15,7 @@ extern "C" {
 void _netemu_respond_to_login_success(struct netemu_packet_buffer* buffer, struct netemu_packet_buffer_item *item, void* arg);
 void _netemu_respond_to_ping(struct netemu_packet_buffer* buffer, struct netemu_packet_buffer_item *item, void* arg);
 void _netemu_respond_to_user_join(struct netemu_packet_buffer* buffer, struct netemu_packet_buffer_item *item, void* arg);
+void _netemu_respond_to_user_leave(struct netemu_packet_buffer* buffer, struct netemu_packet_buffer_item *item, void* arg);
 void _netemu_respond_to_game_created(struct netemu_packet_buffer* buffer, struct netemu_packet_buffer_item *item, void* arg);
 void _netemu_respond_to_player_joined(struct netemu_packet_buffer* buffer, struct netemu_packet_buffer_item *item, void* arg);
 void _netemu_respond_to_buffered_values(struct netemu_packet_buffer* buffer, struct netemu_packet_buffer_item *item, void* arg);
