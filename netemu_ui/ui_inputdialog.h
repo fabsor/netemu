768bd588bf7260d63039efdf2ea8e714a8167fa9
Emil Nordén <emil@emil-laptop.(none)>
2010-05-12 20:41:04 +0200

    Modified the mainwindow to use the new error function which returns a string format of the last error

--- netemu_ui/ui_inputdialog.h
+++ netemu_ui/ui_inputdialog.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'inputdialog.ui'
 **
-** Created: Sun May 9 21:13:49 2010
+** Created: Wed May 12 20:33:42 2010
 **      by: Qt User Interface Compiler version 4.6.2
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
